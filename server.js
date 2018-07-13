365d38d189e61a57686e3afe6ba0e765f42024e7
Aric1088 <ariczhuang@gmail.com>
2018-07-13 01:07:58 -0400

    Fix gitignore

--- server.js
+++ server.js
@@ -1,39 +1,46 @@
-const
-    portAudio = require('naudiodon');
-    lame = require('lame');
-    express = require('express');
-
+const portAudio = require('naudiodon');
+const lame = require('lame');
+const express = require('express');
+var app = express()
+var connectionLog = []
 var ai = new portAudio.AudioInput({
   channelCount: 2,
   sampleFormat: portAudio.SampleFormat16Bit,
   sampleRate: 48000,
   deviceId: 1
 });
-
-var encoder = new lame.Encoder({
+encoder = new lame.Encoder({
   channels: 2,
   bitDepth: 16,
   sampleRate: 48000,
-
   bitRate: 320,
   outSampleRate: 48000,
   mode: lame.STEREO
 })
 
-
-
-ai.on('error', err => console.error);
-ai.pipe(encoder);
-ai.start();
-
-var app = express()
+function initializeStream(input, output) {
+  input.on('error', err => console.error);
+  input.pipe(output);
+  input.start();
+}
+initializeStream(ai, encoder);
+app.get('/analytics', function(req, res) {
+  let result = '<table>';
+  for (var i = 0; i < connectionLog.length; i++) {
+    result += "<tr><td>" + connectionLog[i] + "</td><td>";
+  }
+  result += '</table>';
+  res.send(result)
+});
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 app.use(express.static("static"));
-app.get('/stream.mp3', function (req, res) {
+app.get('/stream.mp3', function(req, res) {
+  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  connectionLog.push(ip)
   res.set({
     'Content-Type': 'audio/mpeg',
     'Transfer-Encoding': 'chunked'
