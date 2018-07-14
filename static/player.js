adb3839f37614daac761c268bfcfa6fc532c9566
Aric Zhuang <ariczhuang@gmail.com>
2018-07-13 21:54:16 -0500

    test

--- static/player.js
+++ static/player.js
@@ -1,1 +1,31 @@
-var latency;function setDevice(){-1!=(navigator.userAgent.indexOf("Opera")||navigator.userAgent.indexOf("OPR"))?alert("Opera"):-1!=navigator.userAgent.indexOf("Chrome")?latency=1.45:-1!=navigator.userAgent.indexOf("Safari")?latency=0:-1!=navigator.userAgent.indexOf("Firefox")?latency=0:-1<navigator.userAgent.indexOf("Edge")?latency=0:-1!=navigator.userAgent.indexOf("MSIE")||1==!!document.documentMode?alert("IE"):alert("unknown")}function play(){setTimeout(function(){var e=document.getElementById("background");e.currentTime=latency,e.play()},0)}setDevice();
+var seek;
+var isPlaying;
+function setDevice() {
+  -1 != (navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf("OPR"))
+    ? alert("Opera")
+    : -1 != navigator.userAgent.indexOf("Chrome")
+      ? seek = 1.45
+      : -1 != navigator.userAgent.indexOf("Safari")
+        ? seek = 0
+        : -1 != navigator.userAgent.indexOf("Firefox")
+          ? seek = 0
+          : -1 < navigator.userAgent.indexOf("Edge")
+            ? seek = 0
+            : -1 != navigator.userAgent.indexOf("MSIE") || 1 == !!document.documentMode
+              ? alert("IE")
+              : alert("unknown")
+}
+function play() {
+  if (!isPlaying){
+  setTimeout(function() {
+    var e = document.getElementById("background");
+    e.currentTime = seek
+    e.play()
+    isPlaying = True
+  }, 0)
+}else{
+  e.pause()
+  e.currentTime = 0
+}
+}
+setDevice();
