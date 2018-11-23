8d6ca6121d8e789abc0a9f61e478c5a90eb9e2e1
Aric Zhuang <ariczhuang@gmail.com>
2018-11-23 13:59:31 -0500

    cleanup

--- server.js
+++ server.js
@@ -1,75 +1,86 @@
-(function (){
-const Store = require('electron-store');
-const portAudio = require('naudiodon');
-const lame = require('lame');
-const express = require('express');
-const datetime = require('node-datetime');
-const path = require('path');
-var app = express()
-var connectionLog = {}
-const store = new Store();
-device_id = 1
-if (store.get('device_id') !== undefined){
-  device_id = app.listen(store.get('device_id'));
-}else{
-  device_id = 1
-  store.set('device_id', 1);
-}
-var ai = new portAudio.AudioInput({
-  channelCount: 2,
-  sampleFormat: portAudio.SampleFormat16Bit,
-  sampleRate: 48000,
-  deviceId: (store.get('device_id'))
-});
-var encoder = new lame.Encoder({
-  channels: 2,
-  bitDepth: 16,
-  sampleRate: 48000,
-  bitRate: 320,
-  outSampleRate: 48000,
-  mode: lame.STEREO
-})
-
-function initializeStream(input, output) {
-  input.on('error', err => console.error);
-  input.pipe(output);
-  input.start();
-}
-initializeStream(ai, encoder);
-app.get('/analytics', function(req, res) {
-  let result = '<table>';
-  for (const [key, value] of Object.entries(connectionLog)) {
-    result += "<tr><td>[" + key + "]</td><td>" + value + " connected.<td></tr>";
+(function() {
+  const Store = require("electron-store");
+  const portAudio = require("naudiodon");
+  const lame = require("lame");
+  const express = require("express");
+  const datetime = require("node-datetime");
+  const path = require("path");
+  var app = express();
+  var connectionLog = {};
+  const store = new Store();
+  device_id = 1;
+  if (store.get("device_id") !== undefined) {
+    device_id = app.listen(store.get("device_id"));
+  } else {
+    device_id = 1;
+    store.set("device_id", 1);
   }
-  result += '</table>';
-  res.send(result)
-});
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-app.use(express.static(path.join(__dirname, "static")));
-app.get('/stream', function(req, res) {
-  var ip = (req.headers['x-forwarded-for'] || req.connection.remoteAddress);
-  var dt = datetime.create(Date.now());
-  connectionLog[dt.format('m/d/Y H:M:S')] = ip.substring(7);
-  res.set({
-    'Content-Type': 'audio/mpeg',
-    'Transfer-Encoding': 'chunked'
+  //Instantiate a new portaudio device for input audio stream
+  var ai = new portAudio.AudioInput({
+    channelCount: 2,
+    sampleFormat: portAudio.SampleFormat16Bit,
+    sampleRate: 48000,
+    deviceId: store.get("device_id")
   });
-  encoder.pipe(res);
-})
-var server;
-if (store.get('port') !== undefined){
-  console.log('helo')
-  console.log(store.get('port'))
-  server = app.listen(store.get('port'));
-}else{
-  console.log('whomst')
-  store.set('port', 80);
-  console.log('server has started')
-  server = app.listen(80);
-}
-module.exports = server;
-}());
+  var encoder = new lame.Encoder({
+    channels: 2,
+    bitDepth: 16,
+    sampleRate: 48000,
+    bitRate: 320,
+    outSampleRate: 48000,
+    mode: lame.STEREO
+  });
+  //Initialize the stream, given the input and output
+  function initializeStream(input, output) {
+    input.on("error", err => console.error);
+    input.pipe(output);
+    input.start();
+  }
+
+  initializeStream(ai, encoder);
+
+  //Endpoint for history of connected streaming devices
+  app.get("/analytics", function(req, res) {
+    let result = "<table>";
+    for (const [key, value] of Object.entries(connectionLog)) {
+      result +=
+        "<tr><td>[" + key + "]</td><td>" + value + " connected.<td></tr>";
+    }
+    result += "</table>";
+    res.send(result);
+  });
+  //Middleware for cross origin requests
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+  });
+  //Serve the index.html page as well as player.js
+  app.use(express.static(path.join(__dirname, "static")));
+
+  //Stream endpoint
+  app.get("/stream", function(req, res) {
+    var ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+    var dt = datetime.create(Date.now());
+    connectionLog[dt.format("m/d/Y H:M:S")] = ip.substring(7);
+    res.set({ "Content-Type": "audio/mpeg", "Transfer-Encoding": "chunked" });
+    encoder.pipe(res);
+  });
+
+  //set port
+  var server;
+  if (store.get("port") !== undefined) {
+    console.log("helo");
+    console.log(store.get("port"));
+    server = app.listen(store.get("port"));
+  } else {
+    store.set("port", 80);
+    console.log("server has started");
+    server = app.listen(80);
+  }
+  //Exported for easy electron packaging
+  module.exports = server;
+})();
--- static/player.js
+++ static/player.js
@@ -1,43 +1,45 @@
 var seek;
-var host = "ariczhuang.ddns.net:80"
+var host = "ariczhuang.ddns.net:80";
 var isPlaying = !1;
+//Detect proper device to account for stream latency
 function setDevice() {
-  -1 != (navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf("OPR"))
+  -1 !=
+  (navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf("OPR"))
     ? alert("Opera")
     : -1 != navigator.userAgent.indexOf("Chrome")
-      ? seek = 1.45
-      : -1 != navigator.userAgent.indexOf("Safari")
-        ? seek = 0
-        : -1 != navigator.userAgent.indexOf("Firefox")
-          ? seek = 0
-          : -1 < navigator.userAgent.indexOf("Edge")
-            ? seek = 0
-            : -1 != navigator.userAgent.indexOf("MSIE") || 1 == !!document.documentMode
-              ? alert("IE")
-              : alert("unknown")
+    ? (seek = 1.45)
+    : -1 != navigator.userAgent.indexOf("Safari")
+    ? (seek = 0)
+    : -1 != navigator.userAgent.indexOf("Firefox")
+    ? (seek = 0)
+    : -1 < navigator.userAgent.indexOf("Edge")
+    ? (seek = 0)
+    : -1 != navigator.userAgent.indexOf("MSIE") || 1 == !!document.documentMode
+    ? alert("IE")
+    : alert("unknown");
 }
 navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform)
-  ? document.body.style.backgroundImage = "url(mobileImage.jpg)"
-  : document.body.style.backgroundImage = "url(desktopImage.jpg)";
+  ? (document.body.style.backgroundImage = "url(mobileImage.jpg)")
+  : (document.body.style.backgroundImage = "url(desktopImage.jpg)");
 var e = document.getElementById("background");
+
+//Play, pause functionality
 function play() {
   if (isPlaying) {
     document.getElementById("player");
-    document.getElementById("player").className = "fa fa-play fa-2x",
-    document.getElementById("player").style.marginLeft = "0px";
+    (document.getElementById("player").className = "fa fa-play fa-2x"),
+      (document.getElementById("player").style.marginLeft = "0px");
     var e = document.getElementById("background");
-    e.pause(),
-    e.src = "",
-    isPlaying = !1
+    e.pause(), (e.src = ""), (isPlaying = !1);
   } else
     setTimeout(function() {
       var e = document.getElementById("background");
-      document.getElementById("player").className = "fa fa-pause",
-      document.getElementById("player").style.marginLeft = "-12px",
-      e.src = "http://" + host + "/stream",
-      e.currentTime = seek,
-      e.play(),
-      isPlaying = !0
-    }, 0)
+      (document.getElementById("player").className = "fa fa-pause"),
+        (document.getElementById("player").style.marginLeft = "-12px"),
+        (e.src = "http://" + host + "/stream"),
+        (e.currentTime = seek),
+        e.play(),
+        (isPlaying = !0);
+    }, 0);
 }
 setDevice();
